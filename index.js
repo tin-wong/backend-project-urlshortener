537725e5b6a6966d212a3a8cce8a402e256c8cd8
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-14 03:41:56 -0700

    Set up mongodb & mongoose

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Install and Set Up Mongoose
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+async function clearCollections() {
+    console.log("test")
+    const collections = mongoose.connection.collections;
+  
+    await Promise.all(Object.values(collections).map(async (collection) => {
+        await collection.deleteMany({}); // an empty mongodb selector object ({}) must be passed as the filter argument
+    }));
+  }
+
+clearCollections()
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
