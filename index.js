6b73e5d4e7f47f2f96eb0485c96fb21539581484
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-16 06:49:18 -0700

    Add comments

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ const urlSchema = new mongoose.Schema({
 
 const Url = mongoose.model('Url', urlSchema);
 
-//Delete all Documents
+// Delete all exisiting documents in the database
 Url.deleteMany({}, (error, mongooseDeleteResult) => {
     if(error) return console.error(error);
     console.log(mongooseDeleteResult);
 });
 
+// Count the number of exisiting documents. Use async and await to wait for the result before proceeding. 
 let total = 0; 
 async function mongooseCount(){
     const count = await Url.countDocuments({});
     return count;
 };
-
 async function documentsCount() {
     const count = await mongooseCount();
     total = count;
@@ -59,20 +59,23 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// 
+// Return a JSON reponse from POST /api/shorturl route 
 app.post('/api/shorturl', (req, res, next) => {
+    // Use regex to get the domain name from the URL. Strip http prefix and the / file directory out of the URL. 
     const domainNameRegex = /(https?:\/\/)(([a-z]|[A-Z]|[0-9]|\-|\.)*)/ig;
     const domainNameMatch = domainNameRegex.exec(req.body.url);
     if(domainNameMatch === null){
         // Use return instead of next() so it won't continue to domainNameMatch[2]
         return res.json({error: 'invalid url'});
     }
-
+    // Use dns.lookup to validate the domain name. It has to be declared last because
+    // it can't be returned/escaped from the rest of the codes in the function.
     dns.lookup(domainNameMatch[2], (err, address, family) => {
-
         if(err) return res.json({error: "invalid URL"});
+        // Use mongoose to find a document with {original_url: req.body.url}
         Url.findOne({original_url: req.body.url}, (err, doc) => {
             if(err) return console.error(err);
+            // If not found, save it as a new document and return it as a JSON response. 
             if(doc === null){
                 documentsCount();
                 let newUrl = new Url({original_url: req.body.url, short_url: total + 1});
@@ -81,25 +84,31 @@ app.post('/api/shorturl', (req, res, next) => {
                 })
                 return res.json({original_url: req.body.url, short_url: total + 1});
             } else {
+                // If found, return the result as a JSON response. 
                 return res.json({original_url: doc.original_url, short_url: doc.short_url});
             }
         })
     });
 });
 
-// 
+// Redirect to a website with from GET /api/shorturl/:short_url route
 app.get('/api/shorturl/:short_url', (req, res, next) => {
+    // Use regex to check if req.params.short_url is a number
     const regex = /^\d*$/;
     if(regex.test(req.params.short_url)){
+        // Use mongoose to find req.params.short_url
         Url.findOne({short_url: req.params.short_url}, (err, doc) => {
             if(err) return console.error(err);
             if(doc === null){
-                return res.json({error:"No short URL found for the given input"});
+                // If not found, return an error.
+                return res.json({error: "No short URL found for the given input"});
             } else {
+                // If found, redirect to the original_url using the short_url reference 
                 return res.redirect(doc.original_url);
             }
         });
     } else {
+        // If req.params.short_url is not a number, return "Wrong format"
         return res.json({error: "Wrong format"});
     }
 })
